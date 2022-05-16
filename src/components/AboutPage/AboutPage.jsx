a1314c19460c87c98c8e1da1a167c01b1691140f
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-05-16 16:24:50 -0500

    adds downward arrow for scroll direction

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Box, Button, Typography } from '@mui/material'
+import { Box, Button, Typography, Container } from '@mui/material'
+import ArrowDownwardRoundedIcon from '@mui/icons-material/ArrowDownwardRounded';
 
 function AboutPage() {
   return (<>
-    <Box>
-      <Typography variant="subtitle2" color="info">WHAT'S ARE TRADITIONAL</Typography>
-      <img src="/public/images/DeskSides_Logotype.png"/>
+
+    <Box sx={{backgroundColor: "#F6F3E3"}}>
+      <br />
+      <Typography 
+      align="center" 
+      variant="subtitle2" 
+      color="info">WHAT ARE TRADITIONAL</Typography>
+
+      <img src="/images/DeskSides_Logotype.png"/>
+
+      <br />
+      <ArrowDownwardRoundedIcon align="center" color="info" fontSize="large" />
     </Box>
     <Box>
       <p>This about page is for anyone to read!</p>
     </Box>
-  </>
+   </>
   );
 }
 
