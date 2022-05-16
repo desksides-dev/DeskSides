2596668c97f0d1e7a1378839a43c814ceadc13c7
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-05-16 10:07:33 -0500

    updates database entry and references to users table

--- database.sql
+++ database.sql
@@ -103,25 +103,3 @@ VALUES ('Fashion'), ('Beauty'), ('Domestic Lifestyle'), ('Health and Wellness'),
        ('Parenting'), ('Food and Beverage'), ('Interior Design and Architecture'), 
        ('Entertainment and Culture'), ('Fitness'), ('Business'), ('Sustainability'), ('Sports'), 
        ('Consumer Goods');
-
-
-
-
-INSERT INTO "publications" ("pub_title")
-VALUES ('Vogue'), ('Architectural Digest'), ('Allure'), ('Bon Appetit'), ('Conde Nast Traveler'), 
-       ('Epicurious'), ('Glamour'), ('GQ'), ('SELF'), ('Tatler'), ('Teen Vogue'), ('The New Yorker'), 
-       ('Vanity Fair'), ('Cosmopolitan'), ('Country Living'), ('Delish'), ('ELLE'), ('ELLE Decor'), 
-       ('Esquire'), ('Food Network Magazine'), ('Good Housekeeping'), ('Harper’s Bazaar'), 
-       ('HGTV Magazine'), ('House Beautiful'), ('Men’s Health'), ('Oprah Daily'), ('Prevention'), 
-       ('Redbook'), ('Runner’s World'), ('Seventeen'), ('Town & Country'), ('Veranda'), ('Woman’s Day'), 
-       ('Women’s Health'), ('VeryWell'), ('Health'), ('Parents'), ('All Recipes'), ('The Spruce Eats'), 
-       ('EatingWell'), ('Food & Wine'), ('Better Homes & Gardens'), ('Martha Stewart Living'), 
-       ('The Spruce'), ('Real Simple'), ('MyDomaine'), ('Domino'), ('Southern Living'), ('Byrdie'), 
-       ('Bustle'), ('Popsugar'), ('Purewow'), ('InStyle'), ('Shape'), ('Travel + Leisure'), ('Treehugger'), 
-       ('People'), ('Coastal Living'), ('Traditional Home');
-
-INSERT INTO "markets" ("market_name")
-VALUES ('Fashion'), ('Beauty'), ('Domestic Lifestyle'), ('Health and Wellness'), ('Travel'), ('Tech'), 
-       ('Parenting'), ('Food and Beverage'), ('Interior Design and Architecture'), 
-       ('Entertainment and Culture'), ('Fitness'), ('Business'), ('Sustainability'), ('Sports'), 
-       ('Consumer Goods');
