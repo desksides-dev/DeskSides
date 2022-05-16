37a2d3c014a4e1a83733a18d8731e28348495f09
Zach Carter <thezachcarter@gmail.com>
2022-05-16 16:30:48 -0500

    implement mui stack

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,31 +1,51 @@
-import React from 'react';
-import './Footer.css';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link'
-import Container from '@mui/material/Container'
+import React from "react";
+import "./Footer.css";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
 
 function Footer() {
-
   return (
-  <Container>
-    <Link href="/">COPYRIGHT © 2022</Link>
-    {/* <Link href="/">TERMS & CONDITIONS</Link> */}
-    <Box
-        component="img"
-        sx={{
-          height: 358,
-          width: 788,
-          maxHeight: { xs: 233, md: 167 },
-          maxWidth: { xs: 350, md: 250 },
-        }}
-        alt="DeskSides Logo"
-        src="https://uc7fe49a364077335cf864344de0.previews.dropboxusercontent.com/p/thumb/ABj9f23MFY4dSDwAv7JIfrGgqJEO9GhATrVHIUsRYIJ3UmEl6uVYZfdMdC90On98uzWw1DnU4IzP9dd8dxTRGVpGRexUs39xzjX0xzOemt8bafJ8m5nNeHs02RhFKCNkBNqWNJl8GGvt_qU4g99raX4YQMQR7-DRbaosCo0AL9GRXkDm3ZLc18LvvF9LYqTiTQoM6PnqQ-L4kMva5Em6F71k4CAfsKkS7pm-emEM7IYRyrPwh5paNVO8BfSFPjndBvWAyxvOssqkBbuMRcRcOO7g0H_E0zwFWHjuCc1wj-A5wMQ9GpRO9Ha1KB4FwAg9o3rudcm7_Eag2vSCzZI_EnpMVPb0URLQ3VsQecKEzkLy_HLX7SxbL8NehDjsZ9bB9ZE/p.png"
-      />    
-    <Link href="/">FACEBOOK</Link>
-    <Link href="/">INSTAGRAM</Link>
-    <Link href="/">CONTACT US</Link>
-  </Container>
-)
+      <Stack 
+        direction="row" 
+        spacing={12}
+        justifyContent="center"
+        alignItems="center"
+        sx={{ border: '1px solid' }}
+      >
+        <Box>
+          <Link href="/">COPYRIGHT © 2022</Link>
+        </Box>
+        
+        <Box>
+          <Link href="/">TERMS & CONDITIONS</Link>
+        </Box>
+       
+        <Box
+          component="img"
+          sx={{
+            height: 89,
+            width: 197,
+          }}
+          alt="DeskSides Logo"
+          src="/images/desksides-logo.svg"
+        />
+        
+        <Box>
+          <Link href="/">FACEBOOK</Link>
+        </Box>
+        
+        <Box>
+          <Link href="/">INSTAGRAM</Link>
+        </Box>
+        
+        <Box>
+          <Link href="/">CONTACT US</Link>
+        </Box>
+
+      </Stack>
+  );
 }
 
 export default Footer;
