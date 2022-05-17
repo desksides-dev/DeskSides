04b26df1996280b1ea7bed791585a4f447427f61
Zach Carter <thezachcarter@gmail.com>
2022-05-17 08:45:15 -0500

    update color scheme

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import "./Footer.css";
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
 import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 function Footer() {
   return (
@@ -11,16 +12,16 @@ function Footer() {
         spacing={12}
         justifyContent="center"
         alignItems="center"
-        sx={{ border: '1px solid', }}
+        sx={{ border: '1px #546D1D solid', }}
       >
         <Box>
-          <h5>COPYRIGHT © 2022</h5>
+          <Link sx={{ textDecoration: 'none', color: '#546D1D' }} >COPYRIGHT © 2022</Link>
         </Box>
         
         
         <Box>
           {/* this link may need to be removed and spacing modified. it is currently a placeholder  */}
-          <Link sx={{ textDecoration: 'none' }} href="/">TERMS & CONDITIONS</Link>
+          <Link sx={{ textDecoration: 'none', color: '#546D1D' }} href="/">TERMS & CONDITIONS</Link>
         </Box>
        
         <Box
@@ -28,8 +29,8 @@ function Footer() {
           href="/"
           component="img"
           sx={{
-            height: 89,
-            width: 197,
+            height: '5em',
+            width: '11em',
           }}
           alt="DeskSides Logo"
           src="/images/desksides-logo.svg"
@@ -37,16 +38,16 @@ function Footer() {
         
         <Box>
           {/* this link needs official facebook address */}
-          <Link sx={{ textDecoration: 'none' }} href="/">FACEBOOK</Link>
+          <Link sx={{ textDecoration: 'none', color: '#546D1D' }} href="/">FACEBOOK</Link>
         </Box>
         
         <Box>
-          <Link sx={{ textDecoration: 'none' }} href="https://www.instagram.com/desksides/" target="_blank">INSTAGRAM</Link>
+          <Link sx={{ textDecoration: 'none', color: '#546D1D' }} href="https://www.instagram.com/desksides/" target="_blank">INSTAGRAM</Link>
         </Box>
         
         <Box>
           {/* this link needs official email address */}
-          <Link sx={{ textDecoration: 'none' }} href="mailto:email@example.com">CONTACT US</Link>
+          <Button variant="contained" sx={{ backgroundColor: '#FFAA45' }} href="mailto:email@example.com">CONTACT US</Button>
         </Box>
 
       </Stack>
