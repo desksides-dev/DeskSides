4f61bbccccdc161dca7712154430060380a95b7b
Maggie Jenkins <adair.jenkins@gmail.com>
2022-05-18 18:00:41 -0500

    GET route in router complete

--- server/routes/journalists.router.js
+++ server/routes/journalists.router.js
@@ -7,7 +7,18 @@ const {rejectUnauthenticated} = require('../modules/authentication-middleware')
 router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('journalists GET route');
 
-    const queryText = ``
+    const queryText = `SELECT * FROM "users"
+                       JOIN "journalists_brands" ON "users".id = "journalists_brands".journalists_id
+                       WHERE "journalists_brands".brands_id = $1;
+                       `
+    const values = [req.user.id];
+    pool.query(queryText, values)
+        .then(result => {
+            res.send(result.rows);
+        })
+        .catch(error => {
+            console.log('error in journalists GET')
+        })
 })
 
 module.exports = router;
