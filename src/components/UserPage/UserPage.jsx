d6d2d8382616f94f03410bc7732259c158175c3e
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-05-20 12:54:33 -0500

    adds external links to open in new tab

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -17,10 +17,10 @@ function UserPage() {
         align: "center",
         justifyContent: "left",
         textAlign: "left",
-        height: "35vh",
+        height: "30vh",
         pl: "10vw",
         pr: "45vw",
-        py: "10vh"
+        py: "5vh"
       }} >
 
         <Typography
@@ -43,9 +43,9 @@ function UserPage() {
         align: "center",
         justifyContent: "center",
         textAlign: "center",
-        height: "auto",
-        px: "10vw",
-        py: "5vh"
+        height: "50vh",
+        px: "5vw",
+        py: "10vh"
       }} >
         <IconButton 
         size="small" 
@@ -53,7 +53,7 @@ function UserPage() {
         onClick={() => {window.open(`${user.calendar_link}`)}}>
           <Box
             component="img"
-            sx={{ width: "10em", m: "5vmax" }}
+            sx={{ width: "7em", m: "1vmax" }}
             alt="down arrow"
             src="/images/calendar-icon-red.svg"
           />
@@ -64,22 +64,45 @@ function UserPage() {
         sx={{ p: "2vmax"  }}>
           <Box
             component="img"
-            sx={{ width: "10em", m: "5vmax" }}
+            sx={{ width: "7em", m: "1vmax" }}
             alt="down arrow"
             src="/images/message-icon-red.svg"
           />
         </IconButton>
+
         <IconButton 
         size="small" 
         sx={{ p: "2vmax"  }}
         onClick={() => {window.open(`${user.brand_assets_link}`)}}>
           <Box
             component="img"
-            sx={{ width: "10em", height: "10em", m: "5vmax" }}
+            sx={{ width: "7em", height: "7em", m: "1vmax" }}
             alt="down arrow"
             src="/images/folder-icon-red.svg"
           />
           </IconButton>
+
+          <IconButton 
+        size="small" 
+        sx={{ p: "2vmax"  }}>
+          <Box
+            component="img"
+            sx={{ width: "7em", m: "1vmax" }}
+            alt="down arrow"
+            src="/images/message-icon-red.svg"
+          />
+        </IconButton>
+
+        <IconButton 
+        size="small" 
+        sx={{ p: "2vmax"  }}>
+          <Box
+            component="img"
+            sx={{ width: "7em", m: "1vmax" }}
+            alt="down arrow"
+            src="/images/message-icon-red.svg"
+          />
+        </IconButton>
       </Box>
     
     </>
