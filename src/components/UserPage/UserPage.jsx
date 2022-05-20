60ec4eedbf74d6dbff6a52e8dda834ec8e0f5286
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-05-20 13:51:54 -0500

    builds out basic brand dashboard information with clickable buttons to links/redirects

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import { Box, Typography, IconButton } from '@mui/material'
+import { styled } from '@mui/material/styles';
+import { Box, Typography, IconButton, Tooltip, tooltipClasses } from '@mui/material';
+import { Light } from '@mui/icons-material';
 
 function UserPage() {
   const history = useHistory();
 
   const user = useSelector((store) => store.user);
+
+  const LightTooltip = styled(({ className, ...props }) => (
+    <Tooltip {...props} classes={{ popper: className }} />
+  ))(({ theme }) => ({
+    [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: '#DC4634',
+      color: '#F6F3E3',
+      fontSize: 11,
+    },
+  }));
 
 
   return (
@@ -47,64 +58,79 @@ function UserPage() {
         px: "5vw",
         py: "10vh"
       }} >
-        <IconButton 
-        size="small" 
-        sx={{ p: "2vmax" }}
-        onClick={() => {window.open(`${user.calendar_link}`)}}>
-          <Box
-            component="img"
-            sx={{ width: "7em", m: "1vmax" }}
-            alt="down arrow"
-            src="/images/calendar-icon-red.svg"
-          />
-        </IconButton>
-
-        <IconButton 
-        size="small" 
-        sx={{ p: "2vmax"  }}>
-          <Box
-            component="img"
-            sx={{ width: "7em", m: "1vmax" }}
-            alt="down arrow"
-            src="/images/message-icon-red.svg"
-          />
-        </IconButton>
-
-        <IconButton 
-        size="small" 
-        sx={{ p: "2vmax"  }}
-        onClick={() => {window.open(`${user.brand_assets_link}`)}}>
-          <Box
-            component="img"
-            sx={{ width: "7em", height: "7em", m: "1vmax" }}
-            alt="down arrow"
-            src="/images/folder-icon-red.svg"
-          />
+        <LightTooltip
+          title="Your Calendar" placement="top">
+          <IconButton
+            size="small"
+            sx={{ p: "2vmax" }}
+            onClick={() => { window.open(`${user.calendar_link}`) }}>
+            <Box
+              component="img"
+              sx={{ width: "7em", height: "7em", m: "1vmax" }}
+              alt="down arrow"
+              src="/images/calendar-icon-red.svg"
+            />
           </IconButton>
+        </LightTooltip>
 
-          <IconButton 
-        size="small" 
-        sx={{ p: "2vmax"  }}>
-          <Box
-            component="img"
-            sx={{ width: "7em", m: "1vmax" }}
-            alt="down arrow"
-            src="/images/message-icon-red.svg"
-          />
-        </IconButton>
 
-        <IconButton 
-        size="small" 
-        sx={{ p: "2vmax"  }}>
-          <Box
-            component="img"
-            sx={{ width: "7em", m: "1vmax" }}
-            alt="down arrow"
-            src="/images/message-icon-red.svg"
-          />
-        </IconButton>
+        <LightTooltip title="Contact Us" placement='top'>
+          <IconButton
+            size="small"
+            sx={{ p: "2vmax" }}
+            onClick={() => { window.open('mailto: blish@desksides.com') }}>
+            <Box
+              component="img"
+              sx={{ width: "7em", height: "7em", m: "1vmax" }}
+              alt="down arrow"
+              src="/images/mail.svg"
+            />
+          </IconButton>
+        </LightTooltip>
+
+        <LightTooltip title="Your Brand Assets" placement="top">
+          <IconButton
+            size="small"
+            sx={{ p: "2vmax" }}
+            onClick={() => { window.open(`${user.brand_assets_link}`) }}>
+            <Box
+              component="img"
+              sx={{ width: "7em", height: "7em", m: "1vmax" }}
+              alt="down arrow"
+              src="/images/folder-icon-red.svg"
+            />
+          </IconButton>
+        </LightTooltip>
+
+        <LightTooltip title="Your Journalist Matches" placement="top">
+          <IconButton
+            size="small"
+            sx={{ p: "2vmax" }}
+            onClick={() => { window.open(history.push('/brand-journalist')) }}>
+            <Box
+              component="img"
+              sx={{ width: "7em", height: "7em", m: "1vmax" }}
+              alt="down arrow"
+              src="/images/people.svg"
+            />
+          </IconButton>
+        </LightTooltip>
+
+        <LightTooltip title="Your Account Settings" placement="top">
+          <IconButton
+            size="small"
+            sx={{ p: "2vmax" }}
+            onClick={() => { window.open(history.push('/brand-journalist')) }}>
+            <Box
+              component="img"
+              sx={{ width: "7em", height: "7em", m: "1vmax" }}
+              alt="down arrow"
+              src="/images/settings.svg"
+            />
+          </IconButton>
+        </LightTooltip>
       </Box>
-    
+
     </>
   );
 }
