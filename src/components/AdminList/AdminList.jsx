aa7055953a40185cbdf11c215a2109d8efa9f134
Zach Carter <thezachcarter@gmail.com>
2022-05-23 11:00:32 -0500

    add table sort labels to adminList

--- src/components/AdminList/AdminList.jsx
+++ src/components/AdminList/AdminList.jsx
@@ -30,52 +30,62 @@ function AdminList() {
     history.push("/adminItem");
   };
 
-
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell sx={{ fontWeight: "bolder" }}>First Name</TableCell>
-            <TableCell sx={{ fontWeight: "bolder" }}>Last Name</TableCell>
-            <TableCell sx={{ fontWeight: "bolder" }}>City</TableCell>
-            <TableCell sx={{ fontWeight: "bolder" }}>State</TableCell>
-            <TableCell sx={{ fontWeight: "bolder" }}>User Type</TableCell>
-            <TableCell sx={{ fontWeight: "bolder" }}>Approved</TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {/* short circuit to prevent race condition, only executes .map when array is not empty */}
-          {adminUsers.length > 0 &&
-            adminUsers?.map((user) => (
-              <TableRow
-                key={user.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {user.first_name}
-                </TableCell>
-                <TableCell>{user.last_name}</TableCell>
-                <TableCell>{user.city}</TableCell>
-                <TableCell>{user.state}</TableCell>
-                <TableCell>{user.user_type}</TableCell>
-                <TableCell>{user.approved.toString()}</TableCell>
-                <TableCell>
-                  <Button
-                    variant="contained"
-                    color="info"
-                    sx={{ fontFamily: "Lato, sansSerif" }}
-                    onClick={() => handleDetails(user)}
-                  >
-                    Details
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell key="name" sx={{ fontWeight: "bolder" }}>
+                <TableSortLabel>Name</TableSortLabel>
+              </TableCell>
+              <TableCell key="city" sx={{ fontWeight: "bolder" }}>
+                <TableSortLabel>City</TableSortLabel>
+              </TableCell>
+              <TableCell key="state" sx={{ fontWeight: "bolder" }}>
+                <TableSortLabel>State</TableSortLabel>
+              </TableCell>
+              <TableCell key="type" sx={{ fontWeight: "bolder" }}>
+                <TableSortLabel>User Type</TableSortLabel>
+              </TableCell>
+              <TableCell sx={{ fontWeight: "bolder" }}>
+                <TableSortLabel>Approved</TableSortLabel>
+              </TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {/* short circuit to prevent race condition, only executes .map when array is not empty */}
+            {adminUsers.length > 0 &&
+              adminUsers?.map((user) => (
+                <TableRow
+                  key={user.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {user.first_name} {user.last_name}
+                  </TableCell>
+                  <TableCell>{user.city}</TableCell>
+                  <TableCell>{user.state}</TableCell>
+                  <TableCell>{user.user_type}</TableCell>
+                  <TableCell>{user.approved.toString()}</TableCell>
+                  <TableCell>
+                    <Button
+                      disableElevation
+                      variant="contained"
+                      color="info"
+                      sx={{ fontFamily: "Lato, sansSerif" }}
+                      onClick={() => handleDetails(user)}
+                    >
+                      Details
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
 
