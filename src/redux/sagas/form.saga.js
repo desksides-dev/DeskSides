0c14345e0f4db68aa6bb2dd470010f6e35f64a61
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-05-24 10:45:06 -0500

    formats and comments edit page

--- src/redux/sagas/form.saga.js
+++ src/redux/sagas/form.saga.js
@@ -61,12 +61,24 @@ function* postBrand(action) {
     }
 }
 
+function* putBrand(action) {
+    const state = action.payload
+    console.log('from *putBrand: ', state)
+
+    try {
+        yield axios.put('/api/form/brands', state)
+    } catch (error) {
+        console.log('error in *postBrand: ', error)
+    }
+}
+
 // main Saga to act as a conductor to correct path:
 function* formSaga() {
     yield takeLatest('GET_PUBS', getPubs);
     yield takeLatest('GET_MARKETS', getMarkets);
     yield takeLatest('J_ASSESS', postJournalist);
     yield takeLatest('B_ASSESS', postBrand);
+    yield takeLatest('B_EDIT_ASSESS', putBrand);
 }
 
 export default formSaga;
